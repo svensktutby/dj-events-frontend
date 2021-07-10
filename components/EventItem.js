370f00cafd493206616ee7c28de90a4546bb658d
svensktutby <svensk.tut.by@gmail.com>
2021-07-10 11:00:40 +0300

    feat: connect to strapi API

--- components/EventItem.js
+++ components/EventItem.js
@@ -7,7 +7,7 @@ export default function EventItem({ evt }) {
     <div className={styles.event}>
       <div className={styles.img}>
         <Image
-          src={evt.image || '/images/event-default.png'}
+          src={evt.image.formats.thumbnail.url || '/images/event-default.png'}
           width={170}
           height={100}
           alt={evt.name}
@@ -16,7 +16,7 @@ export default function EventItem({ evt }) {
 
       <div className={styles.info}>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
         <h3>{evt.name}</h3>
       </div>
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -25,14 +25,19 @@ export default function EventPage({ evt }) {
         </div>
 
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
 
         <h1>{evt.name}</h1>
 
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} alt={evt.name} />
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+              alt={evt.name}
+            />
           </div>
         )}
 
@@ -52,7 +57,7 @@ export default function EventPage({ evt }) {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events`);
   const events = await res.json();
 
   const paths = events.map((evt) => ({
@@ -66,7 +71,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
   const events = await res.json();
 
   return {
